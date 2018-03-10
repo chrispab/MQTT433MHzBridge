577c504dfcf3f9e179e2f3a88443fc71d2f2d274
chrispab <cbattisson@gmail.com>
2018-03-10 23:29:55 +0000

    twaek response

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ char pass[] = "a new router can solve many problems"; // your network password
 int status = WL_IDLE_STATUS;
 
 #include "SoftwareSerial.h"
-SoftwareSerial Serial1(7, 6); // RX, TX
+SoftwareSerial ESPSerial(7, 6); // RX, TX
 
 WiFiEspServer server(80);
 NewRemoteTransmitter transmitter(282830, 4);
@@ -34,7 +34,7 @@ void printWifiStatus()
     Serial.println(" dBm");
 }
 
-void processGetString(String &getLine)
+boolean processRequest(String &getLine)
 {
     char *command;
     char *param;
@@ -43,9 +43,12 @@ void processGetString(String &getLine)
     const char s[] = " /?=&"; // delimeter chars for strtok
     char *token;
     char *ptr;
-    long ret;
+    //long ret;
     bool newState = false;
 
+    //now chck if a GET was received previously
+    //vars for sockt and state should be present
+    //if so then do socket control command
     //poss url format
     ///test/demo_form.php?name1=value1&name2=value2
 
@@ -53,9 +56,12 @@ void processGetString(String &getLine)
     //Switch TEST_433_Control "TEST_433_Control"  <switch> {http=">[ON:GET:http://192.168.0.230/45=ZON:on] >[OFF:GET:http://192.168.0.230/45=ZOFF:off]" }
     //http://192.168.0.230/45=ZON
 
+    // format of expected url http://192.168.0.230/<command|device>/<id>/<state>
+    // e.g http://192.168.0.230/socket/12/1  - socket 12 (of 0-15), ON
+
     // extract socket num and on or off param
-    //if (getLine.indexOf("GET /") != -1)
-    if (1)
+    if (getLine.indexOf("GET /") != -1)
+    //if (1)
     { // get command detected
         Serial.println("GET / - found in response");
 
@@ -84,22 +90,17 @@ void processGetString(String &getLine)
 
     //now send command to socket
 
-    
-
-    //Serial.print("String part is : ");
-    //Serial.println(ptr);
-
     //check for type of command received
     if (!strcmp("switch", command))
     {
-    //get the integer val of param char array - param - the socket number
+        //get the integer val of param char array - param - the socket number
         int socketNum = strtol(param, &ptr, 10);
         //Serial.print("The number(ul int) is : ");
-        Serial.println(ret); 
-        
+        //Serial.println(socketNum);
+
         //calc if command is true or false - converst state string to int 0, or 1
-        
-        if (!strcmp(value,"ON") )
+
+        if (!strcmp(value, "ON"))
         { //mustbe ON command
             newState = true;
         } //else nmust be OFF
@@ -107,23 +108,57 @@ void processGetString(String &getLine)
         {
             newState = false;
         }
-        Serial.print("++++ Send command to socket : ");
+        Serial.print(">>>>> Send command to socket : ");
         Serial.print(socketNum);
         Serial.print(", new  State : ");
         Serial.println(newState);
 
         digitalWrite(5, newState); // GET /H turns the LED on
         transmitter.sendUnit(socketNum, newState);
+        return true;
     }
-    else{
+    else
+    {
         Serial.println("Request not recognised");
+        return false;
     }
+}
+
+void sendResponse(WiFiEspClient client)
+{
+    // send a standard http response header
+    // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+    // and a content-type so the client knows what's coming, then a blank line:
+    client.println("HTTP/1.1 200 OK");
+    // client.println("Content-Type: text/html");
+    // client.println();
+    // // the content of the HTTP response follows the header:
+    // client.println("<!DOCTYPE HTML>");
+
+    // for (int i = 0; i < 16; i++)
+    // {
+    // client.print("<a href=/");
+    // client.print(i);
+    // client.print("=ON> Turn ON Socket : ");
+    // client.print(i);
+    // client.print("</a><br>");
+    // client.println();
+
+    // client.print("<a href=/");
+    // client.print(i);
+    // client.print("=OFF> Turn OFF Socket : ");
+    // client.print(i);
+    // client.print("</a><br>");
+    //  client.println();
+    // }
+
+    //client.println();
 }
 
 void setup()
 { //Initialize serial and wait for port to open:
-    //Serial.begin(115200);
-    Serial.begin(9600);
+    Serial.begin(115200);
+    //Serial.begin(9600);
     while (!Serial)
     {
         ; // wait for serial port to connect. Needed for Leonardo only
@@ -133,9 +168,10 @@ void setup()
     // initialize serial for ESP module
     // TODO - try setting esp-01 baud rate higher for better response
     //find fastets baud rate soft serial can manage confortably
-    Serial1.begin(9600);
+    //try with 31250 baud
+    ESPSerial.begin(9600);
     // initialize ESP module
-    WiFi.init(&Serial1);
+    WiFi.init(&ESPSerial);
 
     // check for the presence of the shield:
     if (WiFi.status() == WL_NO_SHIELD)
@@ -167,75 +203,41 @@ void loop()
     // listen for incoming clients
     WiFiEspClient client = server.available();
 
-    if (client)
+    if (client) // keep checking for new client connection coming in
     {
         Serial.println("new client");
         String currentLine = ""; // make a String to hold incoming data from the client
-        String GetRequest = "";  //storage for get request line
+        String request = "";     //storage for get request line
+        char c;
         while (client.connected())
         {
             if (client.available())
             {
-                char c = client.read();
-                Serial.write(c);
-                if (c == '\n')
+                c = client.read(); //get next char rxed
+                Serial.write(c);   //echo toserial monitor
+                if (c == '\n')     //could be 1st-end of line  or 2nd - end of request \n in request
                 {
-
-                    // if the current line is blank, and you have got two newline characters in a row.
-                    // that's the end of the client HTTP request, so send a response:
+                    // if the current line is empty, means this is 2nd \n char in a row.
+                    // therefore that's the end of the client HTTP request
                     if (currentLine.length() == 0)
-                    //send a response then process the command from the get line if there was a GET?
-                    { // send a standard http response header
-                        // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-                        // and a content-type so the client knows what's coming, then a blank line:
-                         client.println("HTTP/1.1 200 OK");
-                        // client.println("Content-Type: text/html");
-                        // client.println();
-                        // // the content of the HTTP response follows the header:
-                        // client.println("<!DOCTYPE HTML>");
-
-                        // for (int i = 0; i < 16; i++)
-                        // {
-                            // client.print("<a href=/");
-                            // client.print(i);
-                            // client.print("=ON> Turn ON Socket : ");
-                            // client.print(i);
-                            // client.print("</a><br>");
-                            // client.println();
-
-                            // client.print("<a href=/");
-                            // client.print(i);
-                            // client.print("=OFF> Turn OFF Socket : ");
-                            // client.print(i);
-                            // client.print("</a><br>");
-                           //  client.println();
-                        // }
-
-                         //client.println();
-
-                        //now chck if a GET was received previously
-                        //vars for sockt and state should be present
-                        //if so then do socket control command
-                        if (GetRequest.indexOf("GET /") != -1)
-                        {
-                            processGetString(GetRequest);
-                        }
-
-                        // break out of the while loop:
+                    {
+                        //request has been rxed, see if we can process it
+                        processRequest(request);
+                        //send a response
+                        sendResponse(client);
+                        // break out of the while loop to terminate connection
                         break;
                     }
-                    else
-                    { // \n rxed and currentLine is not empty so process it
-                        // Check to see what the client request was and process
-                        //look to see if there was a GET request on the current line
-                        if (currentLine.indexOf("GET /") != -1) //last line was a get request with params
+                    else                                        // 1st \n rxed - end of current line - and currentLine is not empty
+                    {                                           //look to see if current line has  a GET request in it
+                        if (currentLine.indexOf("GET /") != -1) //last line was a get line
                         {
-                            GetRequest = currentLine;
+                            request = currentLine; // store current line in request as it is the GET line
                         }
-                        currentLine = "";
+                        currentLine = ""; // empty current line ready for next line rxed
                     }
                 }
-                else if (c != '\r')
+                else if (c != '\r') // ignore if \r
                 {
                     // you've gotten a character on the current line - other than \n or \r
                     currentLine += c; // add current rxed char it to the end of the currentLine
