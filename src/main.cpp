474f4bef83b696c0d292913bc5e5b5495f32b65e
chrispab <cbattisson@gmail.com>
2018-03-31 18:58:06 +0100

    working

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,17 @@
 // create new bboard to test
 //work only using wifiesp lib as connection to net
 
-
 //#include <SPI.h>
 #include <WiFiEsp.h>
 #include <NewRemoteTransmitter.h>
 
 #include <PubSubClient.h>
 
+//forward decs
+void printWifiStatus();
+boolean processRequest(String &getLine);
+void sendResponse(WiFiEspClient client);
+void listenForClients(void);
 
 char ssid[] = "notwork";                              // your network SSID (name)
 char pass[] = "a new router can solve many problems"; // your network password
@@ -20,9 +24,7 @@ SoftwareSerial ESPSerial(7, 6); // RX, TX
 
 WiFiEspServer server(80);
 
-
-IPAddress mqttserver(172, 16, 0, 2);
-
+IPAddress mqttserver(192, 168, 0, 200);
 
 //WiFiEspClient client = server.available();
 //EthernetClient ethClient;
@@ -31,23 +33,182 @@ NewRemoteTransmitter transmitter(282830, 4);
 byte socket = 3;
 bool state = false;
 #define LEDPIN 5
-//Supported baud rates are 300, 600, 1200, 2400, 4800, 9600, 14400, 
-//19200, 28800, 31250, 38400, 57600, and 115200. 
+//Supported baud rates are 300, 600, 1200, 2400, 4800, 9600, 14400,
+//19200, 28800, 31250, 38400, 57600, and 115200.
 #define ESP_BAUD 9600
 
-
-void callback(char* topic, byte* payload, unsigned int length) {
-  // handle message arrived mqtt pubsub
-
+void callback(char *topic, byte *payload, unsigned int length)
+{
+    // handle message arrived mqtt pubsub
+    Serial.println("Rxed a mesage from broker : ");
+    
+    //char payload[255];
+    payload[length] = '\0';
+    String s = String((char*)payload);
+    
+    Serial.print( s );
+    Serial.println("--EOP");
 }
 
-
 WiFiEspClient WiFiEClient;
-PubSubClient client(mqttserver, 1883, callback, WiFiEClient);
+PubSubClient psclient(mqttserver, 1883, callback, WiFiEClient);
 
+void setup()
+{ //Initialize serial monitor port to PC and wait for port to open:
+    Serial.begin(115200);
+    //Serial.begin(9600);
+    while (!Serial)
+    {
+        ; // wait for serial port to connect. Needed for Leonardo only
+    }
+    pinMode(LEDPIN, OUTPUT); // set the LED pin mode
 
+    // initialize serial for ESP module
+    // TODO - try setting esp-01 baud rate higher for better response
+    //find fastets baud rate soft serial can manage confortably
+    //try with 31250 baud 8mhz xtal
+    //try 57600 with 16mhz xtal
 
+    //open at 9600 (default)
+    ESPSerial.begin(ESP_BAUD);
+    //get current baud for this session
+    Serial.println("Sending an AT command...");
+    ESPSerial.println("AT+UART_CUR?");
+    delay(30);
 
+    while (ESPSerial.available())
+    {
+        String inData = ESPSerial.readStringUntil('\n');
+        Serial.println("Got reponse from ESP8266: " + inData);
+    }
+
+    //set working baud for this session
+    Serial.println("Sending an AT command...");
+    ESPSerial.println("AT+UART_CUR?"); //ADD SET COMMAND
+    delay(30);
+
+    while (ESPSerial.available())
+    {
+        String inData = ESPSerial.readStringUntil('\n');
+        Serial.println("Got reponse from ESP8266: " + inData);
+    }
+    //query current settings
+    //Serial.println(ESPSerial.println("AT+UART_CUR?"));
+
+    //////////////////////////////////////////////////////
+    //  ESPSerial.println("AT+UART_CUR=14400,8,1,0,0");
+    //  delay(300);
+    //  ESPSerial.end();
+    // // delay(300);
+    //  ESPSerial.begin(14400);
+    //////////////////////////////////////////////////
+
+    //show new working baud
+    ESPSerial.println("AT+UART_CUR?");
+    delay(30);
+    while (ESPSerial.available())
+    {
+        String inData = ESPSerial.readStringUntil('\n');
+        Serial.println("current working BAUD response from ESP-01: " + inData);
+    }
+    delay(300);
+    //set baud rate for this session to 57600 - not saved in flash
+    // AT+UART_CUR=115200,8,1,0,3
+
+    // initialize ESP module
+    WiFi.init(&ESPSerial);
+    // check for the presence of the shield:
+    if (WiFi.status() == WL_NO_SHIELD)
+    {
+        Serial.println("WiFi shield not present");
+        // don't continue:
+        while (true)
+            ;
+    }
+
+    // attempt to connect to Wifi network:
+    while (status != WL_CONNECTED)
+    {
+        Serial.print("Attempting to connect to SSID: ");
+        Serial.println(ssid);
+        // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+        status = WiFi.begin(ssid, pass);
+
+        // wait 10 seconds for connection:
+        delay(3000);
+    }
+    server.begin();
+    // you're connected now, so print out the status:
+    printWifiStatus();
+
+    if (psclient.connect("arduinoClient", "testuser", "testpass"))
+    {
+        psclient.publish("outTopic", "hello world");
+        psclient.subscribe("Outside_Sensor/tele/SENSOR");
+    }
+}
+
+void loop()
+{
+    //psclient.loop();
+    listenForClients();
+}
+
+void listenForClients(void)
+{
+    // listen for incoming clients
+    WiFiEspClient client = server.available();
+
+    if (client) // keep checking for new client connection coming in
+    {
+        Serial.println("new client!!");
+        String currentLine = ""; // make a String to hold incoming data from the client
+        String request = "";     //storage for get request line
+        char c;
+        while (client.connected())
+        {
+            if (client.available())
+            {
+                c = client.read(); //get next char rxed
+                //think about storing line rather thatmn printing each char after rx
+                Serial.write(c); //echo toserial monitor
+                if (c == '\n')   //could be 1st-end of line  or 2nd - end of request \n in request
+                {
+                    // if the current line is empty, means this is 2nd \n char in a row.
+                    // therefore that's the end of the client HTTP request
+                    if (currentLine.length() == 0)
+                    {
+                        //request has been rxed, see if we can process it
+                        processRequest(request);
+                        //send a response
+                        sendResponse(client);
+                        // break out of the while loop to terminate connection
+                        break;
+                    }
+                    else                                        // 1st \n rxed - end of current line - and currentLine is not empty
+                    {                                           //look to see if current line has  a GET request in it
+                        if (currentLine.indexOf("GET /") != -1) //last line was a get line
+                        {
+                            request = currentLine; // store current line in request as it is the GET line
+                        }
+                        currentLine = ""; // empty current line ready for next line rxed
+                    }
+                }
+                else if (c != '\r') // ignore if \r
+                {
+                    // you've gotten a character on the current line - other than \n or \r
+                    currentLine += c; // add current rxed char it to the end of the currentLine
+                }
+            }
+        }
+        // give the web browser time to receive the data
+        delay(20);
+
+        // close the connection:
+        client.stop();
+        Serial.println("client disconnected!!");
+    }
+}
 void printWifiStatus()
 {
     // print the SSID of the network you're attached to:
@@ -185,140 +346,4 @@ void sendResponse(WiFiEspClient client)
     // }
 
     //client.println();
-}
-
-void setup()
-{ //Initialize serial monitor port and wait for port to open:
-    Serial.begin(115200);
-    //Serial.begin(9600);
-    while (!Serial)
-    {
-        ; // wait for serial port to connect. Needed for Leonardo only
-    }
-    pinMode(LEDPIN, OUTPUT); // set the LED pin mode
-
-    // initialize serial for ESP module
-    // TODO - try setting esp-01 baud rate higher for better response
-    //find fastets baud rate soft serial can manage confortably
-    //try with 31250 baud 8mhz xtal
-    //try 57600 with 16mhz xtal
-    ESPSerial.begin(ESP_BAUD);
-    Serial.println("Sending an AT command...");
-    ESPSerial.println("AT+UART_CUR?");
-    delay(30);
-    while (ESPSerial.available())
-    {
-        String inData = ESPSerial.readStringUntil('\n');
-        Serial.println("Got reponse from ESP8266: " + inData);
-    }
-    //query current settings
-    //Serial.println(ESPSerial.println("AT+UART_CUR?"));
-
-    //////////////////////////////////////////////////////
-    //  ESPSerial.println("AT+UART_CUR=14400,8,1,0,0");
-    //  delay(300);
-    //  ESPSerial.end();
-    // // delay(300);
-    //  ESPSerial.begin(14400);
-    //////////////////////////////////////////////////
-
-
-    ESPSerial.println("AT+UART_CUR?");
-    delay(30);
-    while (ESPSerial.available())
-    {
-        String inData = ESPSerial.readStringUntil('\n');
-        Serial.println("Got NEW reponse from ESP8266: " + inData);
-    }
-    delay(300);
-    //set baud rate for this session to 57600 - not saved in flash
-    // AT+UART_CUR=115200,8,1,0,3
-
-    // initialize ESP module
-    WiFi.init(&ESPSerial);
-    // check for the presence of the shield:
-    if (WiFi.status() == WL_NO_SHIELD)
-    {
-        Serial.println("WiFi shield not present");
-        // don't continue:
-        while (true)
-            ;
-    }
-
-    // attempt to connect to Wifi network:
-    while (status != WL_CONNECTED)
-    {
-        Serial.print("Attempting to connect to SSID: ");
-        Serial.println(ssid);
-        // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
-        status = WiFi.begin(ssid, pass);
-
-        // wait 10 seconds for connection:
-        delay(3000);
-    }
-    server.begin();
-    // you're connected now, so print out the status:
-    printWifiStatus();
-
-      if (client.connect("arduinoClient", "testuser", "testpass")) {
-    client.publish("outTopic","hello world");
-    client.subscribe("inTopic");
-  }
-}
-
-void loop()
-{
-      client.loop();
-
-    // listen for incoming clients
-    WiFiEspClient client = server.available();
-
-    if (client) // keep checking for new client connection coming in
-    {
-        Serial.println("new client");
-        String currentLine = ""; // make a String to hold incoming data from the client
-        String request = "";     //storage for get request line
-        char c;
-        while (client.connected())
-        {
-            if (client.available())
-            {
-                c = client.read(); //get next char rxed
-                Serial.write(c);   //echo toserial monitor
-                if (c == '\n')     //could be 1st-end of line  or 2nd - end of request \n in request
-                {
-                    // if the current line is empty, means this is 2nd \n char in a row.
-                    // therefore that's the end of the client HTTP request
-                    if (currentLine.length() == 0)
-                    {
-                        //request has been rxed, see if we can process it
-                        processRequest(request);
-                        //send a response
-                        sendResponse(client);
-                        // break out of the while loop to terminate connection
-                        break;
-                    }
-                    else                                        // 1st \n rxed - end of current line - and currentLine is not empty
-                    {                                           //look to see if current line has  a GET request in it
-                        if (currentLine.indexOf("GET /") != -1) //last line was a get line
-                        {
-                            request = currentLine; // store current line in request as it is the GET line
-                        }
-                        currentLine = ""; // empty current line ready for next line rxed
-                    }
-                }
-                else if (c != '\r') // ignore if \r
-                {
-                    // you've gotten a character on the current line - other than \n or \r
-                    currentLine += c; // add current rxed char it to the end of the currentLine
-                }
-            }
-        }
-        // give the web browser time to receive the data
-        //delay(1);
-
-        // close the connection:
-        client.stop();
-        Serial.println("client disonnected");
-    }
 }
